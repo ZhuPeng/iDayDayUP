489de023c77648020abf3e1d94cc7ecfea640702
Peng Zhu <zhupeng02@meituan.com>
2020-12-19 14:05:11 +0800

    Update fmt

--- js/parse.py
+++ js/parse.py
@@ -19,7 +19,12 @@ for part in c.split('. ### '):
     if 'Back to Top](#table-of-contents)**' not in part:
         continue
     q = 'JavaScript: ' + part.split('\n')[0] + '?'
-    a = '\n'.join(part.split('\n')[1:-3])
+    arr = []
+    for l in part.split('\n')[1:-3]:
+        if len(l) > 0 and l[0:3] == '   ':
+            l = l[3:]
+        arr.append(l)
+    a = '\n'.join(arr)
     a = a.replace('](images/', '](https://7465-test-3c9b5e-books-1301492295.tcb.qcloud.la/images/compress_')
     print 'question:', q
     print 'answer:', a
